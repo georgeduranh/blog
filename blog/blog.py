ebe18acf357f2a12c23b477a51ef2b135c18aec5
georgeduranh <duran.jorgeivan@gmail.com>
2022-06-23 19:29:20 -0500

    first version like button, pending limiting the likes per user and the dislike feature

--- blog/blog.py
+++ blog/blog.py
@@ -13,7 +13,7 @@ bp = Blueprint('blog', __name__)
 def index():
     db = get_db()
     posts = db.execute(
-        'SELECT p.id, title, body, created, author_id, username'
+        'SELECT p.id, title, body, created, author_id, username, likes'
         ' FROM post p JOIN user u ON p.author_id = u.id'
         ' ORDER BY created DESC'
     ).fetchall()
@@ -48,7 +48,7 @@ def create():
 
 def get_post(id, check_author=True):
     post = get_db().execute(
-        'SELECT p.id, title, body, created, author_id, username'
+        'SELECT p.id, title, body, created, author_id, username, likes'
         ' FROM post p JOIN user u ON p.author_id = u.id'
         ' WHERE p.id = ?',
         (id,)
@@ -107,3 +107,25 @@ def delete(id):
 def show(id):
     post = get_post(id)
     return render_template('blog/show.html', post=post)
+
+
+@bp.route('/<int:id>/like', methods=('GET', 'POST'))
+@login_required
+def like(id):
+    post = get_post(id)
+    like = post['likes']
+    
+    if request.method == 'POST':
+                
+        db = get_db()
+        like = like + 1
+        
+        db.execute(
+                'UPDATE post SET likes = ?'
+                ' WHERE id = ?',
+                (like, id)
+            )
+        db.commit()
+        return redirect(url_for('blog.index'))
+
+    return render_template('blog/like.html', post=post)
